1dbfcd9a303d5680d5e1379bc6c355e557858311
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-10-12 22:02:27 -0700

    updates

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-require("console.table")
+require("console.table");
 
 
 const connection = mysql.createConnection({
     host: "localhost",
 ​
-    // Your port; if not 3306
+    
     port: 3306,
 ​
-    // Your username
+    
     user: "root",
 ​
-    // Your password
+  
     password: "password",
     database: "employee_trackerDB"
 });
-​
+
 connection.connect(function (err) {
     if (err) throw err;
-    askQuestions();
+    console.log("Test");
+    // askQuestions();
 });
 
-function askQuestions() {
-    inquirer.prompt([
-        {
-            name: "choice",
-            type: "list",
-            message: "what would you like to do?",
-            choices: ["add department", "add role", "add employee", "view department", "view role", "view employees", "update employee roles" "QUIT"]
-        }
-    ]).then(function (answers) {
-        if (answers.choice === "Search by Artist") {
-            searchByArtist();
-        }else if (answers.choice === "Search by Song") {
-            searchBySong();
-        }else if (answers.choice === "Search by Year Range") {
-            searchByYearRange();
-        }else if (answers.choice === "See Multi Track Artists") {
-        seeMultiTrackArtists();
-    }else {
-            console.log("bye!");
-            connection.end();
-        }
-    })
-}
+// function askQuestions() {
+//     inquirer.prompt([
+//         {
+//             name: "choice",
+//             type: "list",
+//             message: "what would you like to do?",
+//             choices: ["add department", "add role", "add employee", "view department", "view role", "view employees", "update employee roles", "QUIT"]
+//         }
+//     ]).then(function (answers) {
+//         console.log("hello, world!");
+//     //     if (answers.choice === "add department") {
+//     //         addDepartment();
+//     //     }else if (answers.choice === "add role") {
+//     //         addRole();
+//     //     }else if (answers.choice === "add employee") {
+//     //         addEmployee();
+//     //     }else if (answers.choice === "update employee roles") {
+//     //     updateEmployeeRoles();
+//     // }else {
+//     //         console.log("bye!");
+//     //         connection.end();
+//     //     }
+//     })
+// }
+
